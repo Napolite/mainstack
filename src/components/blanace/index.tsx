0aa2d1e4415eec5af7ba3f275ea91979315992bd
Constantine <Okonemmanuelmma@gmail.com>
2025-04-10 17:52:46 +0100

    balances after like days

--- src/components/blanace/index.tsx
+++ src/components/blanace/index.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { CiCircleInfo } from "react-icons/ci";
 
 function Balances() {
+  const [balances, setBalances] = React.useState<{
+    ledger: number;
+    totalPayout: number;
+    totalRevenue: number;
+    pendingPayout: number;
+  }>({
+    ledger: 0,
+    totalPayout: 55080,
+    totalRevenue: 175580,
+    pendingPayout: 0.0,
+  });
+
   return (
-    <div>
-      <div></div>
+    <div className="h-[360px] w-[360271 flex flex-col justify-between items-center">
+      <div className="w-[271px] h-[66px] flex items-center justify-between">
+        <div>
+          <div className="text-[14px] text-[#56616b]">Ledger Balance</div>
+          <div className="text-[#131316] text-[26px] font-[700]">
+            USD {Number(balances?.pendingPayout.toFixed(2)).toLocaleString()}
+          </div>
+        </div>
+        <CiCircleInfo />
+      </div>
+      <div className="w-[271px] h-[66px] flex items-center justify-between">
+        <div>
+          <div className="text-[14px] text-[#56616b]">Total Payout</div>
+          <div className="text-[#131316] text-[26px] font-[700]">
+            USD {Number(balances?.totalPayout.toFixed(2)).toLocaleString()}
+          </div>
+        </div>
+        <CiCircleInfo />
+      </div>
+      <div className="w-[271px] h-[66px] flex items-center justify-between">
+        <div>
+          <div className="text-[14px] text-[#56616b]">Total Revenue</div>
+          <div className="text-[#131316] text-[26px] font-[700]">
+            USD {balances?.totalRevenue.toFixed(2).toLocaleString()}
+          </div>
+        </div>
+        <CiCircleInfo />
+      </div>
+      <div className="w-[271px] h-[66px] flex items-center justify-between">
+        <div>
+          <div className="text-[14px] text-[#56616b]">Pending Payout</div>
+          <div className="text-[#131316] text-[26px] font-[700]">
+            USD {balances?.pendingPayout.toFixed(2).toLocaleString()}
+          </div>
+        </div>
+        <CiCircleInfo />
+      </div>
     </div>
   );
 }
--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,3 +1,4 @@
+import Balances from "../components/blanace";
 import GraphAnalytics from "../components/graphAnalytics";
 import Navbar from "../components/navbar";
 
@@ -5,8 +6,9 @@ function MainStack() {
   return (
     <div>
       <Navbar />
-      <div className="mt-[4rem] w-[1159px] mx-auto">
+      <div className="mt-[4rem] w-[72.438rem] mx-auto flex justify-between items-center">
         <GraphAnalytics />
+        <Balances />
       </div>
     </div>
   );
